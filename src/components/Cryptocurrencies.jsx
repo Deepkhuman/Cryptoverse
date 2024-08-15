7b5517bd5a6bfe0b4be2f558cc63c7eb58eb1ecd
Devang Khuman <devangkhuman2003@gmail.com>
2024-08-15 11:26:46 +0530

    removed unusual error

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col } from "antd";
@@ -7,18 +7,30 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const Cryptocurrencies = ({ simplified }) => {
 	const count = simplified ? 10 : 100;
 	const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-	const [cryptos, SetCryptos] = useState(cryptosList?.data.coins);
+	const [cryptos, setCryptos] = useState([]);
+	useEffect(() => {
+		if (cryptosList?.data?.coins) {
+			setCryptos(cryptosList.data.coins);
+		}
+	}, [cryptosList]);
 	if (isFetching) return "Loading...";
 	return (
 		<>
-			<Row gutter={[28, 20]} className="crypto-card-container">
+			<Row gutter={[32, 32]} className="crypto-card-container">
 				{cryptos?.map((currency) => (
-					<Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-						<Link to={`/crypto/${currency.id}`}>
+					<Col
+						xs={24}
+						sm={12}
+						lg={6}
+						className="crypto-card"
+						key={currency.uuid}
+					>
+						<Link to={`/crypto/${currency.uuid}`}>
 							<Card
 								title={`${currency.rank}. ${currency.name}`}
 								extra={<img src={currency.iconUrl} className="crypto-image" />}
-								hoverable
+								size={30}
+								hoverable={true}
 							>
 								<p style={{ padding: "0px 0px 5px 0px" }}>
 									Price : {millify(currency.price)}
